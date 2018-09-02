8b7346439da2910d48ca95aa8b2614a77298e6a0
MrScopes <drewmanrules23@gmail.com>
2018-09-01 20:08:59 -0400

    Discord Bot

--- mybot.js
+++ mybot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Bot by MrScopes - Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '') {
+    msg.delete;
+    msg.('Deleted the last 10 messages...');
+  }
+});
+
+client.login('hidden');
